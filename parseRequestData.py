615108e1168c6126cc8d88606a3259d9a96e04b3
sakura <amyfor7@gmail.com>
2022-12-26 06:00:39 -0800

    feat: remove the TOT_REQ restriction

--- parseRequestData.py
+++ parseRequestData.py
@@ -17,19 +17,14 @@ def parse_dn_file(t):
             fail_dn[CLIENT_IP % fields[0]].append((fields[1], fields[3]))
 
     for j in range(M):
-        TOT_REQ = 100
         reqs = []
         IP = CLIENT_IP % j
         for req in pred_dn[IP]:
-            if TOT_REQ <= 0: break
             for k in range(int(req[1])):
                 reqs.append({'tid': req[0], 'type': 1})
-                TOT_REQ -= 1
         for req in fail_dn[IP]:
-            if TOT_REQ <= 0: break
             for k in range(int(req[1])):
                 reqs.append({'tid': req[0], 'type': 2})
-                TOT_REQ -= 1
 
         print(IP)
         print(len(reqs))
