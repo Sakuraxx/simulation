61cc138b4c2be92174ee7d62abf454de22fad6ec
sakura <amyfor7@gmail.com>
2022-12-27 05:42:51 -0800

    cal average delay

--- cal_profit.py
+++ cal_profit.py
@@ -14,16 +14,15 @@ def cal_tot_delay(filename):
             res += int(fields[-1])
     return res
 
-if __name__ == '__main__':
-    modes = [c for c in CACHE_MODE]
-    modes.append('cloud')
-    # modes = ['ksp', 'cloud']
+
+# 记录平均请求时延
+def cal_avg_req_delay(modes):
     res = {}
     reqCnt = {}
     for mode in modes:
         res[mode] = []
         reqCnt[mode] = []
-        for t in range(3, 4):
+        for t in range(1, T):
             delay = 0
             cnt = 0
             for j in range(M):
@@ -36,18 +35,21 @@ if __name__ == '__main__':
     print('Total Delay:', res)
     print('Total Requests:', reqCnt)
 
-    c_arr = np.array(res['cloud']) / np.array(reqCnt['cloud'])
+    # c_arr = np.array(res['cloud']) / np.array(reqCnt['cloud'])
+    print('Average Request Delay:')
     for mode in modes:
         arr = np.array(res[mode])
-        avgDelay = arr / reqCnt[mode]
-        ans = c_arr - avgDelay
-        ans = np.round(ans, decimals=0) # 平均时延节省量
-        ans = np.round(ans / c_arr * 100, decimals=0) # 平均时延节省率
-        res[mode] = list(ans)
-        print(' ', mode, avgDelay)
-    print('Total Saved Delay Rate:', res)
-    print('=' * 30)
+        avgDelay = np.round(arr / reqCnt[mode], decimals=0)
+        res[mode] = list(avgDelay)
+        print(' ', mode[0:3], '\t', avgDelay)
+    return res
 
-    res_json_file = RES_JSON_FILE % PERIOD
-    with open(res_json_file, 'w') as file:
-            json.dump(res, file)
+
+if __name__ == '__main__':
+    # modes = [c for c in CACHE_MODE]
+    # modes.append('cloud')
+    modes = ['ksp']
+    avg_res = cal_avg_req_delay(modes)
+    avg_json_file = AVG_RES_FILE % PERIOD
+    with open(avg_json_file, 'w') as file:
+            json.dump(avg_res, file)
